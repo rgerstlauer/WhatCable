ea352e24263f81eb3af6e1dcaad520997c08046c
katharina98 <kschuetz@mail.hs-ulm.de>
2020-06-22 17:35:47 +0200

    Bilder verkleinert, Erste Drop Down Funktion, kleine Anpassungen

--- android/app/src/main/java/org/tensorflow/lite/examples/classification/TutorialActivity.java
+++ android/app/src/main/java/org/tensorflow/lite/examples/classification/TutorialActivity.java
@@ -2,12 +2,25 @@ package org.tensorflow.lite.examples.classification;
 
 import androidx.appcompat.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+import com.github.aakira.expandablelayout.ExpandableRelativeLayout;
 
 public class TutorialActivity extends AppCompatActivity {
+    private Button btnOpenOne;
+    private ExpandableRelativeLayout infoFirstEntry;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_tutorial);
+
+        btnOpenOne = (Button)findViewById(R.id.btnOpenOne);
+    }
+
+    public void showInformationOne(View view){
+        infoFirstEntry = (ExpandableRelativeLayout)findViewById(R.id.infoFirstEntry);
+        infoFirstEntry.toggle();
     }
 }
