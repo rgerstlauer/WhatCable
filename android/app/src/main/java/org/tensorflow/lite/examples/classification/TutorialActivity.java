bb1f4823c59a104c1043ac50582787ad8d06e42d
katharina98 <kschuetz@mail.hs-ulm.de>
2020-07-06 14:31:11 +0200

    Handbuch fertig

--- android/app/src/main/java/org/tensorflow/lite/examples/classification/TutorialActivity.java
+++ android/app/src/main/java/org/tensorflow/lite/examples/classification/TutorialActivity.java
@@ -8,27 +8,131 @@ import android.widget.ImageButton;
 import com.github.aakira.expandablelayout.ExpandableRelativeLayout;
 
 public class TutorialActivity extends AppCompatActivity {
-    private ImageButton btnOpenOne;
-    private ImageButton btnOpenTwo;
-    private ExpandableRelativeLayout infoFirstEntry;
-    private ExpandableRelativeLayout infoSecondEntry;
+    private ImageButton btnApp;
+    private ImageButton btnProbability;
+    private ImageButton btnBackground;
+    private ImageButton btnPerspective;
+    private ImageButton btnLight;
+    private ImageButton btnCable;
+    private ImageButton btnConfidence;
+    private ImageButton btnFrame;
+    private ImageButton btnCrop;
+    private ImageButton btnView;
+    private ImageButton btnRotation;
+    private ImageButton btnInference;
+    private ImageButton btnThread;
+    private ImageButton btnModel;
+    private ImageButton btnDevice;
+    private ExpandableRelativeLayout infoApp;
+    private ExpandableRelativeLayout infoProbability;
+    private ExpandableRelativeLayout infoBackground;
+    private ExpandableRelativeLayout infoPerspective;
+    private ExpandableRelativeLayout infoLight;
+    private ExpandableRelativeLayout infoCable;
+    private ExpandableRelativeLayout infoConfidence;
+    private ExpandableRelativeLayout infoFrame;
+    private ExpandableRelativeLayout infoCrop;
+    private ExpandableRelativeLayout infoView;
+    private ExpandableRelativeLayout infoRotation;
+    private ExpandableRelativeLayout infoInference;
+    private ExpandableRelativeLayout infoThread;
+    private ExpandableRelativeLayout infoModel;
+    private ExpandableRelativeLayout infoDevice;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_tutorial);
 
-        btnOpenOne = (ImageButton)findViewById(R.id.btnOpenOne);
-        btnOpenTwo = (ImageButton)findViewById(R.id.btnOpenTwo);
+        btnApp = (ImageButton)findViewById(R.id.btnApp);
+        btnProbability = (ImageButton)findViewById(R.id.btnProbability);
+        btnBackground = (ImageButton)findViewById(R.id.btnBackground);
+        btnPerspective = (ImageButton)findViewById(R.id.btnPerspective);
+        btnLight = (ImageButton)findViewById(R.id.btnLight);
+        btnCable = (ImageButton)findViewById(R.id.btnCabel);
+        btnConfidence = (ImageButton)findViewById(R.id.btnConfidence);
+        btnFrame = (ImageButton)findViewById(R.id.btnFrame);
+        btnCrop = (ImageButton)findViewById(R.id.btnCrop);
+        btnView = (ImageButton)findViewById(R.id.btnView);
+        btnRotation = (ImageButton)findViewById(R.id.btnRotation);
+        btnInference = (ImageButton)findViewById(R.id.btnInference);
+        btnThread = (ImageButton)findViewById(R.id.btnThreads);
+        btnModel = (ImageButton)findViewById(R.id.btnModel);
+        btnDevice = (ImageButton)findViewById(R.id.btnDevice);
     }
 
-    public void showInformationOne(android.view.View View){
-        infoFirstEntry = (ExpandableRelativeLayout)findViewById(R.id.infoFirstEntry);
-        infoFirstEntry.toggle();
+    public void showApp(android.view.View View){
+        infoApp = (ExpandableRelativeLayout)findViewById(R.id.infoApp);
+        infoApp.toggle();
     }
 
-    public void showInformationTwo(android.view.View View){
-        infoSecondEntry = (ExpandableRelativeLayout)findViewById(R.id.infoSecondEntry);
-        infoSecondEntry.toggle();
+    public void showProbability(android.view.View View){
+        infoProbability = (ExpandableRelativeLayout)findViewById(R.id.infoProbability);
+        infoProbability.toggle();
+    }
+
+    public void showBackground(android.view.View View){
+        infoBackground = (ExpandableRelativeLayout)findViewById(R.id.infoBackground);
+        infoBackground.toggle();
+    }
+
+    public void showPerspective(android.view.View View){
+        infoPerspective = (ExpandableRelativeLayout)findViewById(R.id.infoPerspective);
+        infoPerspective.toggle();
+    }
+
+    public void showLight(android.view.View View){
+        infoLight = (ExpandableRelativeLayout)findViewById(R.id.infoLight);
+        infoLight.toggle();
+    }
+
+    public void showCabel(android.view.View View){
+        infoCable = (ExpandableRelativeLayout)findViewById(R.id.infoCabel);
+        infoCable.toggle();
+    }
+
+    public void showConfidence(android.view.View View){
+        infoConfidence = (ExpandableRelativeLayout)findViewById(R.id.infoConfidence);
+        infoConfidence.toggle();
+    }
+
+    public void showFrame(android.view.View View){
+        infoFrame = (ExpandableRelativeLayout)findViewById(R.id.infoFrame);
+        infoFrame.toggle();
+    }
+
+    public void showCrop(android.view.View View){
+        infoCrop = (ExpandableRelativeLayout)findViewById(R.id.infoCrop);
+        infoCrop.toggle();
+    }
+
+    public void showView(android.view.View View){
+        infoView = (ExpandableRelativeLayout)findViewById(R.id.infoView);
+        infoView.toggle();
+    }
+
+    public void showRotation(android.view.View View){
+        infoRotation = (ExpandableRelativeLayout)findViewById(R.id.infoRotation);
+        infoRotation.toggle();
+    }
+
+    public void showInference(android.view.View View){
+        infoInference = (ExpandableRelativeLayout)findViewById(R.id.infoInference);
+        infoInference.toggle();
+    }
+
+    public void showThreads(android.view.View View){
+        infoThread = (ExpandableRelativeLayout)findViewById(R.id.infoThreads);
+        infoThread.toggle();
+    }
+
+    public void showModel(android.view.View View){
+        infoModel = (ExpandableRelativeLayout)findViewById(R.id.infoModel);
+        infoModel.toggle();
+    }
+
+    public void showDevice(android.view.View View){
+        infoDevice = (ExpandableRelativeLayout)findViewById(R.id.infoDevice);
+        infoDevice.toggle();
     }
 }
